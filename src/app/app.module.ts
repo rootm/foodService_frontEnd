76af0caadae37ab6652eb193f591b3c8512abc73
muvindu-tharanga <muvindu.tharanga@my.sliit.lk>
2018-05-29 07:07:56 +0530

    componets added for authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FoodService } from './food.service';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
 
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'home',
+        component: AppComponent
+      }
+    ])
   ],
   providers: [FoodService],
   bootstrap: [AppComponent]
